84d4bf5571277eb97dad1692bc765c024e3b2c80
Tolgay Altiner <tolgay.altiner@web.de>
2024-10-06 11:53:32 +0200

    Transaktionskomponent Erfassung weiter ausgebaut

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import {TransaktionComponent} from "./transaktion/transaktion.component";
+import {MatCard, MatCardContent, MatCardHeader} from "@angular/material/card";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatFormField} from "@angular/material/form-field";
+import { MatLabel } from '@angular/material/form-field';
+import {MatInput} from "@angular/material/input";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatDatepicker, MatDatepickerInput, MatDatepickerToggle} from "@angular/material/datepicker";
+import { MatIconModule } from '@angular/material/icon';
+import { MatNativeDateModule } from '@angular/material/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButton} from "@angular/material/button";
+import {MatRadioButton, MatRadioGroup} from "@angular/material/radio";
 
 const appRoutes: Routes = [
   { path: '', component: TransaktionComponent },
@@ -12,11 +24,28 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-
+    TransaktionComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    MatCard,
+    ReactiveFormsModule,
+    MatCardHeader,
+    MatCardContent,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    BrowserAnimationsModule,
+    MatDatepickerToggle,
+    MatDatepicker,
+    MatDatepickerInput,
+    MatIconModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatButton,
+    MatRadioGroup,
+    MatRadioButton
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/transaktion/transaktion.component.ts
+++ src/app/transaktion/transaktion.component.ts
@@ -1,12 +1,26 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {  Component } from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-transaktion',
-  standalone: true,
-  imports: [],
   templateUrl: './transaktion.component.html',
   styleUrl: './transaktion.component.scss'
 })
+
 export class TransaktionComponent {
+  transaktionForm = new FormGroup({
+    'einnahme': new FormControl('', Validators.required),
+    'datumEinnahme': new FormControl('', Validators.required),
+    'notiz': new FormControl(''),
+    'tranksaktionsArt': new FormControl('', Validators.required)
+  })
+
+  einnahmeChange() {
+    console.log('einnahme entered')
+  }
+
+  notizChange() {
+    console.log('notiz entered');
+  }
 
 }
